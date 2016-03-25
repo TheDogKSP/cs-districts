803b8a7c369980294c050277ac4968e620fd980d
gsteigert <steigert@gmail.com>
2016-03-25 18:16:52 -0300

    Forbid emptying to buildings of other districts

--- DistrictChecker.cs
+++ DistrictChecker.cs
@@ -9,21 +9,23 @@ namespace GSteigertDistricts
 		public static bool IsTransferAllowed(Vector3 source, Vector3 destination,
 		                                     TransferManager.TransferReason material)
 		{
+			DistrictManager districtManager = Singleton<DistrictManager>.instance;
+			byte srcDistrictIdx = districtManager.GetDistrict(source);
+			byte dstDistrictIdx = districtManager.GetDistrict(destination);
+
 			switch(material) {
 			case TransferManager.TransferReason.LeaveCity0:
 			case TransferManager.TransferReason.LeaveCity1:
 			case TransferManager.TransferReason.LeaveCity2:
+				return true;
 			case TransferManager.TransferReason.DeadMove:
 			case TransferManager.TransferReason.GarbageMove:
 			case TransferManager.TransferReason.CriminalMove:
 			case TransferManager.TransferReason.SnowMove:
-				return true;
+				return (dstDistrictIdx == 0 || srcDistrictIdx == dstDistrictIdx);
+			default:
+				return (srcDistrictIdx == 0 || srcDistrictIdx == dstDistrictIdx);
 			}
-
-			DistrictManager districtManager = Singleton<DistrictManager>.instance;
-			byte srcDistrictIdx = districtManager.GetDistrict(source);
-			byte dstDistrictIdx = districtManager.GetDistrict(destination);
-			return (srcDistrictIdx == 0 || srcDistrictIdx == dstDistrictIdx);
 		}
 	}
 
