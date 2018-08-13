8e88ca8400b3d9dbef40f038e83f479c4db98099
egi <daegi01@gmail.com>
2018-08-13 22:44:40 +0200

    bugfix

--- VehiclePatches.cs
+++ VehiclePatches.cs
@@ -3,176 +3,171 @@ using UnityEngine;
 
 namespace DistrictServiceLimit
 {
-    class VehiclePatches
+    [HarmonyPatch(typeof(AmbulanceAI), "StartTransfer")]
+    class AmbulanceAIStartTransferPatch
     {
-        [HarmonyPatch(typeof(AmbulanceAI), "StartTransfer")]
-        class AmbulanceAIStartTransferPatch
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(HearseAI), "StartTransfer")]
-        class HearseAIStartTransferPatch
+    [HarmonyPatch(typeof(HearseAI), "StartTransfer")]
+    class HearseAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(PoliceCarAI), "StartTransfer")]
-        class PoliceCarAIStartTransferPatch
+    [HarmonyPatch(typeof(PoliceCarAI), "StartTransfer")]
+    class PoliceCarAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    Debug.Log("Vehicle transfer allowed, use regular method.");
-                    return true;
-                }
-
-                Debug.Log("Vehicle transfer not allowed, don't do anything.");
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(FireTruckAI), "StartTransfer")]
-        class FireTruckAIStartTransferPatch
+    [HarmonyPatch(typeof(FireTruckAI), "StartTransfer")]
+    class FireTruckAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(GarbageTruckAI), "StartTransfer")]
-        class GarbageTruckAIStartTransferPatch
+    [HarmonyPatch(typeof(GarbageTruckAI), "StartTransfer")]
+    class GarbageTruckAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(SnowTruckAI), "StartTransfer")]
-        class SnowTruckAIStartTransferPatch
+    [HarmonyPatch(typeof(SnowTruckAI), "StartTransfer")]
+    class SnowTruckAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(MaintenanceTruckAI), "StartTransfer")]
-        class MaintenanceTruckAIStartTransferPatch
+    [HarmonyPatch(typeof(MaintenanceTruckAI), "StartTransfer")]
+    class MaintenanceTruckAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(TaxiAI), "StartTransfer")]
-        class TaxiAIStartTransferPatch
+    [HarmonyPatch(typeof(TaxiAI), "StartTransfer")]
+    class TaxiAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(HelicopterAI), "StartTransfer")]
-        class HelicopterAIStartTransferPatch
+    [HarmonyPatch(typeof(HelicopterAI), "StartTransfer")]
+    class HelicopterAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(AmbulanceCopterAI), "StartTransfer")]
-        class AmbulanceCopterAIStartTransferPatch
+    [HarmonyPatch(typeof(AmbulanceCopterAI), "StartTransfer")]
+    class AmbulanceCopterAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(FireCopterAI), "StartTransfer")]
-        class FireCopterAIStartTransferPatch
+    [HarmonyPatch(typeof(FireCopterAI), "StartTransfer")]
+    class FireCopterAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
+    }
 
-        [HarmonyPatch(typeof(PoliceCopterAI), "StartTransfer")]
-        class PoliceCopterAIStartTransferPatch
+    [HarmonyPatch(typeof(PoliceCopterAI), "StartTransfer")]
+    class PoliceCopterAIStartTransferPatch
+    {
+        static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
         {
-            static bool Prefix(ushort vehicleID, ref Vehicle data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+            if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
             {
-                if (DistrictChecker.IsVehicleTransferAllowed(vehicleID, ref data, material, offer))
-                {
-                    return true;
-                }
-
-                return false;
+                return true;
             }
+
+            return false;
         }
     }
 }
